e1e7f651c2af93de735fb1b8d337cdfc94e56184
taktischer | Alex <91153474+taktischer@users.noreply.github.com>
2022-11-11 10:40:49 +0100

    implemented get_parking_lots function
    returns an integer; requires a image_path (string)

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from yolov5 import YOLOv5
-import os
+from configparser import ConfigParser
 
-# set model params
 model_path = "yolov5/weights/yolov5s.pt"
 device = "cpu"
 
 yolov5 = YOLOv5(model_path, device)
 
-for file in os.listdir('./pictures'):
-    if file.endswith(('png', 'jpg', 'jpeg')):
-        results = yolov5.predict(f'./pictures/{file}')
-        predictions = results.pred[0]
-        boxes = predictions[:, :4]
-        scores = predictions[:, 4]
-        categories = predictions[:, 5]
-        print(results)
-        results.show()
+config = ConfigParser()
+config.read('config.ini')
+free_parking_lots = int(config['DEFAULT']['parking_lots'])
+
+
+def get_parking_lots(image_path: str) -> int:
+    global free_parking_lots
+    results = yolov5.predict(image_path)
+    for category in results.pred[0][:, 5]:
+        if int(category) in [2]:  # cars category: 2;
+            free_parking_lots -= 1
+    return free_parking_lots
+
+
+print(get_parking_lots('./pictures/picture_1.jpeg'))
