4dcd114a257920e56c60f5e853f6c5ce7c2f8d92
taktischer | Alex <91153474+taktischer@users.noreply.github.com>
2022-12-15 10:22:02 +0100

    modified directory structure, added requirements.txt and gitignore

--- main.py
+++ main.py
@@ -19,17 +19,16 @@ def get_free_parking_lots(prediction) -> int:
     global free_parking_lots
     area = 0
 
-    for i in range(len(prediction)):
-        obj = prediction[i]
+    for idx, obj in enumerate(prediction):
         width, height = int(obj[2]) - int(obj[0]), int(obj[3]) - int(obj[1])
 
         # define x0/y0 for the base rectangle
         # define x1/y1 for the other rectangle
         # uses the previous area to check which one is the base rectangle
         if area > width * height:
-            x0, x1, y0, y1 = int(prediction[i - 1][0]), int(obj[0]), int(prediction[i - 1][1]), int(obj[1])
+            x0, x1, y0, y1 = int(prediction[idx - 1][0]), int(obj[0]), int(prediction[idx - 1][1]), int(obj[1])
         else:
-            x0, x1, y0, y1 = int(obj[0]), int(prediction[i - 1][0]), int(obj[1]), int(prediction[i - 1][1])
+            x0, x1, y0, y1 = int(obj[0]), int(prediction[idx - 1][0]), int(obj[1]), int(prediction[idx - 1][1])
 
         # change the area to the new area
         area = width * height
@@ -42,10 +41,10 @@ def get_free_parking_lots(prediction) -> int:
 
 # get the prediction value from yolov5
 # image_path, camera_position are required
-def predict_image(image_path: str, camera_position: int):
+def predict_image(camera_position: int):
     # opening picture and bitmap
-    image_0 = Image.open(image_path)
-    bitmap = Image.open(f"pictures/bitmap_parkinglot_{camera_position}.png")
+    image_0 = Image.open(fr"pictures\parkinglots\parkinglot_{camera_position}.png")  # NOQA
+    bitmap = Image.open(fr"pictures\bitmaps\bitmap_{camera_position}.png")
 
     # create new image with size of the picture
     new_im = Image.new('RGBA', (image_0.size[0], image_0.size[1]), (250, 250, 250))
@@ -53,16 +52,15 @@ def predict_image(image_path: str, camera_position: int):
     # paste the bitmap on top
     new_im.paste(image_0, (0, 0), mask=bitmap)
 
-    # modify image_path and save the new image there
-    image_path = image_path.replace('.png', '')
-    new_im.save(f"{image_path}_edit.png")
+    # modify image_path and save the new image in results folder
+    new_im.save(fr".\pictures\results\result_{camera_position}.png")
 
     # get prediction from yolov5
-    results = yolov5.predict(f"{image_path}_edit.png")
+    results = yolov5.predict(fr".\pictures\results\result_{camera_position}.png")
 
     # return the list of tensor values
     return results.pred[0]
 
 
 if __name__ == "__main__":
-    print(get_free_parking_lots(predict_image('./pictures/parkinglot_3.png', 3)))
+    print(get_free_parking_lots(predict_image(3)))
