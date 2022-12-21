d1151f28056c216a48eec7103716c660dec1cde3
taktischer | Alex <91153474+taktischer@users.noreply.github.com>
2022-12-21 16:41:00 +0100

    check which parking space is occupied

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from yolov5 import YOLOv5
-from configparser import ConfigParser
 from PIL import Image
 
 # yolov5 definitions
@@ -7,18 +6,16 @@ model_path = "yolov5/weights/yolov5s.pt"
 device = "cpu"
 yolov5 = YOLOv5(model_path, device)
 
-# load config file
-config = ConfigParser()
-config.read('config.ini')
-free_parking_lots = int(config['DEFAULT']['parking_lots'])
-
 
 # calculate the free parking lots
 # requires a list of tensors from yolov5
-def get_free_parking_lots(prediction) -> int:
-    global free_parking_lots
+def get_occupied_parking_lots(prediction, camera_position) -> int:
     area = 0
+    occupied_parking_spaces = []
 
+    # opens the overlay image, to check which parking spaces are occupied
+    image = Image.open(fr".\pictures\bitmaps\bitmap_{camera_position}_overlay.png")
+    # image.show()
     for idx, obj in enumerate(prediction):
         width, height = int(obj[2]) - int(obj[0]), int(obj[3]) - int(obj[1])
 
@@ -35,8 +32,16 @@ def get_free_parking_lots(prediction) -> int:
 
         # checks if the percentage in the base rectangle is below 25%
         if (x0 - x1) / width * 100 > 25.0 or (y0 - y1) / width * 100 > 25.0:
-            free_parking_lots -= 1
-    return free_parking_lots
+
+            # get the rgba value (overlay image) at the left bottom corner of the rectangle
+            # if the value of red isn't 0 it will be added to the occupied_parking_spaces
+            # the red value is the parking space id
+            position = image.getpixel((int(obj[0]), int(obj[3])))[0]
+            if position != 0:
+                occupied_parking_spaces.append(position)
+
+    image.close()
+    return len(occupied_parking_spaces)
 
 
 # get the prediction value from yolov5
@@ -55,6 +60,9 @@ def predict_image(camera_position: int):
     # modify image_path and save the new image in results folder
     new_im.save(fr".\pictures\results\result_{camera_position}.png")
 
+    # close the image after modification
+    new_im.close()
+
     # get prediction from yolov5
     results = yolov5.predict(fr".\pictures\results\result_{camera_position}.png")
 
@@ -63,4 +71,5 @@ def predict_image(camera_position: int):
 
 
 if __name__ == "__main__":
-    print(get_free_parking_lots(predict_image(3)))
+    camera = 3
+    print(get_occupied_parking_lots(predict_image(camera), camera))
